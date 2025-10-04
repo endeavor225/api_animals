f472985a6190f3999d4217573afb9f3f2e92c6bd
Abraham KONE <abrahamkone12@gmail.com>
2025-10-04 01:17:34 +0000

    Feat : intégration d'adonis-autoswagger pour la documentation de l'API
    - Mise à jour du script de développement dans package.json pour utiliser --watch au lieu de --hmr.
    - Ajout d'adonis-autoswagger comme dépendance pour la génération de la documentation de l'API.
    - Implémentation des routes Swagger dans start/routes.ts pour diffuser la documentation de l'API au format YAML et afficher l'interface utilisateur Swagger.
    - Création d'un nouveau fichier de configuration config/swagger.ts pour définir les paramètres et les métadonnées de la documentation de l'API.

--- app/controllers/animals_controller.ts
+++ app/controllers/animals_controller.ts
@@ -1,34 +1,74 @@
 import Animal from '#models/animal'
-import { AnimalSchema } from '#validators/animal'
+import { AnimalValidator } from '#validators/animal'
 import type { HttpContext } from '@adonisjs/core/http'
 
 export default class AnimalsController {
+  /**
+   * @list
+   * @summary List all animals
+   * @responseBody 200 - <Animal[]>
+   * @responseBody 500 - {message: "Internal server error"}
+   */
   public async list({ response }: HttpContext) {
     const animals = await Animal.all()
     return response.json(animals)
   }
 
+  /**
+   * @show
+   * @summary Show a single animal by ID
+   * @paramPath id - The ID of the animal to retrieve @type(number) - @required
+   * @responseBody 200 - <Animal>
+   * @responseBody 404 - {message: "Animal not found"}
+   * @responseBody 500 - {message: "Internal server error"}
+   */
   public async show({ params, response }: HttpContext) {
     const { id } = params
     const animal = await Animal.findOrFail(id)
     return response.json(animal)
   }
 
+  /**
+   * @create
+   * @summary Create a new animal
+   * @requestBody <AnimalValidator>
+   * @responseBody 201 - <Animal>
+   * @responseBody 422 - {error: [{message: "Validation error", field: "age", rule: "minLength"}]}
+   * @responseBody 500 - {message: "Internal server error"}
+   */
   public async create({ request, response }: HttpContext) {
-    const payload = await request.validateUsing(AnimalSchema)
+    const payload = await request.validateUsing(AnimalValidator)
     const animal = await Animal.create(payload)
     return response.status(201).json(animal)
   }
 
+  /**
+   * @update
+   * @summary Update an existing animal by ID
+   * @paramPath id - The ID of the animal to update @type(number) - @required
+   * @requestBody <AnimalValidator>
+   * @responseBody 200 - <Animal>
+   * @responseBody 404 - {message: "Animal not found"}
+   * @responseBody 422 - {error: [{message: "Validation error", field: "age", rule: "minLength"}]}
+   * @responseBody 500 - {message: "Internal server error"}
+   */
   public async update({ params, request, response }: HttpContext) {
     const { id } = params
-    const payload = await request.validateUsing(AnimalSchema)
+    const payload = await request.validateUsing(AnimalValidator)
     const animal = await Animal.findOrFail(id)
     animal.merge(payload)
     await animal.save()
     return response.json(animal)
   }
 
+  /**
+   * @delete
+   * @summary Delete an animal by ID
+   * @paramPath id - The ID of the animal to delete @type(number) - @required
+   * @responseBody 204 - No Content
+   * @responseBody 404 - {message: "Animal not found"}
+   * @responseBody 500 - {message: "Internal server error"}
+   */
   public async delete({ params, response }: HttpContext) {
     const { id } = params
     const animal = await Animal.findOrFail(id)
--- start/routes.ts
+++ start/routes.ts
@@ -8,10 +8,25 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import AutoSwagger from 'adonis-autoswagger'
+import swagger from '#config/swagger'
+console.log('🚀 ~ swagger:', swagger)
 
 // Import dynamic controller
 const homeController = () => import('#controllers/home_controller')
 const animalsController = () => import('#controllers/animals_controller')
+
+// returns swagger in YAML
+router.get('/swagger', async () => {
+  return AutoSwagger.default.docs(router.toJSON(), swagger)
+})
+
+// Renders Swagger-UI and passes YAML-output of /swagger
+router.get('/docs', async () => {
+  return AutoSwagger.default.ui('/swagger', swagger)
+  // return AutoSwagger.default.scalar("/swagger"); to use Scalar instead. If you want, you can pass proxy url as second argument here.
+  // return AutoSwagger.default.rapidoc("/swagger", "view"); to use RapiDoc instead (pass "view" default, or "read" to change the render-style)
+})
 
 router
   .group(() => {
